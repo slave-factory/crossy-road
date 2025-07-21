3cb2e9ebe919f222d91e7904501707375dfd89e7
kkwon25 <ufks7010031@naver.com>
2025-07-21 23:01:59 +0900

    feat: 충돌 시 텍스트 출력, 색상 입힘, 코인 획득 시 사라짐

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "Player.hpp"
 
 enum class CollisionType {
@@ -16,25 +17,58 @@ bool isColliding(const sf::FloatRect& a, const sf::FloatRect& b) {
 int main() {
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Collision System");
-    window.setVerticalSyncEnabled(false);
+    // window.setVerticalSyncEnabled(false);
+    window.setFramerateLimit(60);
 
+    // 플레이어
     sf::RectangleShape player(sf::Vector2f(50, 50));
     player.setPosition(100, 100);
+    player.setFillColor(sf::Color::Green);
+    float speed = 50.f;
 
+    // 자동차
     sf::RectangleShape car(sf::Vector2f(100, 50));
     car.setPosition(200, 100);
 
-    sf::RectangleShape coin(sf::Vector2f(25, 25));
-    coin.setPosition(500, 100);
+    // 코인
+    sf::CircleShape coin(25.f);
+    coin.setFillColor(sf::Color::Yellow);
+    coin.setPosition(150.f, 200.f);
+    
+    sf::Font font;
+    if (!font.loadFromFile("assets/fonts/Pretendard.ttf")) {
+        std::cerr << "폰트 파일을 불러올 수 없습니다.\n";
+        return -1;
+    }
+    // sf::Text text;
+    sf::Text text("C", font, 40);
+    /* text.setString("C");
+    text.setCharacterSize(40); */
+    text.setFillColor(sf::Color::Black);
+    
+    sf::FloatRect textBounds = text.getLocalBounds();
+    text.setOrigin(textBounds.left + textBounds.width / 2.0f,
+                   textBounds.top + textBounds.height / 2.0f);
 
+    sf::Vector2f circleCenter = coin.getPosition() + sf::Vector2f(coin.getRadius(), coin.getRadius());
+    text.setPosition(circleCenter);
+
+    // (임시) 물
     sf::RectangleShape water(sf::Vector2f(700, 50));
     water.setPosition(0, 400);
+    water.setFillColor(sf::Color::Blue);
+
+    // time
+    sf::Clock clock;
 
     CollisionType collision = CollisionType::Idle;
     bool isDead = false;
+    bool isCoinVisible = true;
     int coinCnt = 0;
 
     while (window.isOpen()) {
+        float dt = clock.restart().asSeconds();
+
         sf::Event event;
         while (window.pollEvent(event))
             if (event.type == sf::Event::Closed)
@@ -44,27 +78,44 @@ int main() {
         isDead = false;
 
         // 이동
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            player.move(7.5, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            player.move(-speed * dt, 0.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            player.move(speed * dt, 0.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            player.move(0.f, -speed * dt);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            player.move(0.f, speed * dt);
+        }
         // 차량 충돌
         if (isColliding(player.getGlobalBounds(), car.getGlobalBounds())) {
             collision = CollisionType::Vehicle;
             isDead = true;
+            std::cout << "차량에 부딪혔습니다.\n";
         // 익사
         } else if (isColliding(player.getGlobalBounds(), water.getGlobalBounds())) {
             collision = CollisionType::Drowned;
             isDead = true;
+            std::cout << "물에 빠졌습니다.\n";
         // 코인
-        } else if (isColliding(player.getGlobalBounds(), coin.getGlobalBounds())) {
+        } else if (isCoinVisible && isColliding(player.getGlobalBounds(), coin.getGlobalBounds())) {
             collision = CollisionType::CoinCollected;
             coinCnt++;
+            isCoinVisible = false;
+            std::cout << "코인을 획득했습니다. 현재 코인 수는 " << coinCnt << "개입니다.\n";
         }
 
         window.clear();
 
         window.draw(player);
         window.draw(car);
-        window.draw(coin);
+        if (isCoinVisible) {
+            window.draw(coin);
+            window.draw(text);
+        }
         window.draw(water);
 
         window.display();
