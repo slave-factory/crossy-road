26cb7326bc106cb6ce7b679c1fd9f60a60ae518d
kkwon25 <ufks7010031@naver.com>
2025-07-21 22:11:07 +0900

    feat: add basic collision detection logic(temporary)

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,79 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Player.hpp"
+
+enum class CollisionType {
+    Idle,
+    Obstacle,
+    Vehicle,
+    Drowned,
+    CoinCollected
+};
+
+bool isColliding(const sf::FloatRect& a, const sf::FloatRect& b) {
+    return a.intersects(b);
+}
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Hello SFML");
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Collision System");
     window.setVerticalSyncEnabled(false);
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("assets/images/lostark_never_die.jpg")) {
-        std::cerr << "Error in Loading Image" << std::endl;
-    }
-    sf::Sprite sprite(texture);
-    
-    // 폰트 설정
-    sf::Font font;
-    if (!font.loadFromFile("assets/fonts/Pretendard.ttf")) {
-        std::cerr << "Error in Loading Font" << std::endl;
-    }
-    sf::Text text("LostArk Naver Pay", font, 50);
+    sf::RectangleShape player(sf::Vector2f(50, 50));
+    player.setPosition(100, 100);
 
-    sf::FloatRect textBounds = text.getLocalBounds();
+    sf::RectangleShape car(sf::Vector2f(100, 50));
+    car.setPosition(200, 100);
 
-    // 기준점을 텍스트 중심으로 설정
-    text.setOrigin(
-        textBounds.left + textBounds.width / 2.f,
-        textBounds.top + textBounds.height / 2.f);
+    sf::RectangleShape coin(sf::Vector2f(25, 25));
+    coin.setPosition(500, 100);
 
-    // 텍스르를 중앙에 배치
-    text.setPosition(
-        window.getSize().x / 2.f,
-        window.getSize().y / 2.f);
+    sf::RectangleShape water(sf::Vector2f(700, 50));
+    water.setPosition(0, 400);
 
-    // 게임 루프 시작
+    CollisionType collision = CollisionType::Idle;
+    bool isDead = false;
+    int coinCnt = 0;
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event))
             if (event.type == sf::Event::Closed)
                 window.close();
 
+        collision = CollisionType::Idle;
+        isDead = false;
+
+        // 이동
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            player.move(7.5, 0);
+        // 차량 충돌
+        if (isColliding(player.getGlobalBounds(), car.getGlobalBounds())) {
+            collision = CollisionType::Vehicle;
+            isDead = true;
+        // 익사
+        } else if (isColliding(player.getGlobalBounds(), water.getGlobalBounds())) {
+            collision = CollisionType::Drowned;
+            isDead = true;
+        // 코인
+        } else if (isColliding(player.getGlobalBounds(), coin.getGlobalBounds())) {
+            collision = CollisionType::CoinCollected;
+            coinCnt++;
+        }
+
         window.clear();
-        window.draw(sprite);
-        window.draw(text);
+
+        window.draw(player);
+        window.draw(car);
+        window.draw(coin);
+        window.draw(water);
+
         window.display();
+
+        if (isDead) {
+            // dead 처리
+            break;
+        }
     }
+
     return 0;
 }
